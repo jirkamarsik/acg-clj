8731574dc5dbbe1b0f15f0805d7978b190db3254
Jiri Marsik <jiri.marsik89@gmail.com>
2013-02-20 23:41:05 +0100

    First working lexicon prototype (no typecheck)

--- src/acg_clj/lexicon.clj
+++ src/acg_clj/lexicon.clj
@@ -42,94 +42,6 @@
     (read-lexicon (line-seq lex-rdr))))
 
 
-(defn syntax<->string [hypertag out]
-  (l/fresh [cats cat wordforms wordform]
-           (l/== hypertag (l/partial-map {:head (l/partial-map {:cat cats
-                                                                :wordform wordforms})}))
-           (l/membero cat cats)
-           (l/membero wordform wordforms)
-           (l/conde [(l/== cat "n")
-                     (l/== out {:type 'N
-                                :syntax->string {:type 'Str
-                                                 :value wordform}})]
-                    [(l/== cat "adj")
-                     (l/== (l/partial-map {:head (l/partial-map {:order ["right"]})})
-                           hypertag)
-                     (n/fresh [x]
-                              (l/== out
-                                    {:type '[-> N N]
-                                     :syntax->string
-                                     ['llam (n/tie x
-                                                   ['app ['app '+ x]
-                                                    {:type 'Str
-                                                     :value wordform}])]}))]
-                    [(l/== cat "det")
-                     (n/fresh [x]
-                              (l/== out
-                                    {:type '[-> N NP]
-                                     :syntax->string
-                                     ['llam (n/tie x
-                                                   ['app ['app '+ {:type 'Str
-                                                                   :value wordform}]
-                                                    x])]}))]
-                    [(l/== cat "v")
-                     (l/== (l/partial-map {:head (l/partial-map {:trans ["false"]})})
-                           hypertag)
-                     (n/fresh [x]
-                              (l/== out
-                                    {:type '[-> NP S]
-                                     :syntax->string
-                                     ['llam (n/tie x
-                                                   ['app ['app '+ x]
-                                                    {:type 'Str
-                                                     :value wordform}])]}))])))
-
-
-(defn unamb-syntax-grammar [hypertag constant]
-  (l/fresh [cats cat wordforms wordform]
-           (l/== hypertag (l/partial-map {:head (l/partial-map {:cat cats
-                                                                :wordform wordforms})}))
-           (l/membero cat cats)
-           (l/membero wordform wordforms)
-           (l/conde [(l/== cat "n")
-                     (l/== out {:type 'N
-                                :syntax->string {:type 'Str
-                                                 :value wordform}})]
-                    [(l/== cat "adj")
-                     (l/== (l/partial-map {:head (l/partial-map {:order ["right"]})})
-                           hypertag)
-                     (n/fresh [x]
-                              (l/== out
-                                    {:type '[-> N N]
-                                     :syntax->string
-                                     ['llam (n/tie x
-                                                   ['app ['app '+ x]
-                                                    {:type 'Str
-                                                     :value wordform}])]}))]
-                    [(l/== cat "det")
-                     (n/fresh [x]
-                              (l/== out
-                                    {:type '[-> N NP]
-                                     :syntax->string
-                                     ['llam (n/tie x
-                                                   ['app ['app '+ {:type 'Str
-                                                                   :value wordform}]
-                                                    x])]}))]
-                    [(l/== cat "v")
-                     (l/== (l/partial-map {:head (l/partial-map {:trans ["false"]})})
-                           hypertag)
-                     (n/fresh [x]
-                              (l/== out
-                                    {:type '[-> NP S]
-                                     :syntax->string
-                                     ['llam (n/tie x
-                                                   ['app ['app '+ x]
-                                                    {:type 'Str
-                                                     :value wordform}])]}))])))
-
-
-
-
 
 
 (defn string-grammar [hypertag constant]
@@ -138,3 +50,139 @@
            (l/membero wordform wordforms)
            (l/== constant {:type 'Str
                            :value wordform})))
+
+(defn unamb-syntax-grammar [hypertag constant]
+  (let [++ {:type '[-> Str [-> Str Str]]
+            :value (with-meta (fn [x]
+                                (fn [y]
+                                  (str x " " y)))
+                     {:print-as '++})}]
+    (l/fresh [string-constant cats cat]
+             (string-grammar hypertag string-constant)
+             (l/== hypertag (l/partial-map {:head (l/partial-map {:cat cats})}))
+             (l/membero cat cats)
+             (l/conde [(l/== cat "n")
+                       (l/== constant {:type 'N
+                                       :unamb-syntax->string string-constant})]
+                      [(l/== cat "adj")
+                       (l/== (l/partial-map {:head (l/partial-map {:order ["right"]})})
+                             hypertag)
+                       (n/fresh [x]
+                                (l/== constant
+                                      {:type '[-> N N]
+                                       :unamb-syntax->string
+                                       ['llam (n/tie x
+                                                     ['app ['app ++ x]
+                                                      string-constant])]}))]
+                      [(l/== cat "det")
+                       (l/fresh [det-type det-types]
+                                (l/== (l/partial-map {:head (l/partial-map {:det_type det-types})})
+                                      hypertag)
+                                (l/membero det-type det-types)
+                                (n/fresh [x]
+                                         (l/== constant
+                                               {:type '[-> N NP]
+                                                :unamb-syntax->string
+                                                ['llam (n/tie x
+                                                              ['app ['app ++ string-constant]
+                                                               x])]})))]
+                      [(l/== cat "v")
+                       (l/== (l/partial-map {:head (l/partial-map {:trans ["false"]})})
+                             hypertag)
+                       (n/fresh [x]
+                                (l/== constant
+                                      {:type '[-> NP S]
+                                       :unamb-syntax->string
+                                       ['llam (n/tie x
+                                                     ['app ['app ++ x]
+                                                      string-constant])]}))]))))
+
+(defn simple-semantics-grammar [hypertag constant]
+  (l/fresh [wordform wordforms cat cats]
+           (l/== hypertag (l/partial-map {:head (l/partial-map {:wordform wordforms
+                                                                :cat cats})}))
+           (l/membero wordform wordforms)
+           (l/membero cat cats)
+           (l/conde [(l/== cat "n")
+                     (l/project [wordform]
+                                (l/== constant {:type '[-> E T]
+                                                :value (with-meta (fn [arg] ; TODO: Shake and bake the function metadata.
+                                                                    (println "Testing if" arg "is a" wordform "."))
+                                                         {:print-as (symbol wordform)})}))]
+                    [(l/== cat "adj")
+                     (l/project [wordform]
+                                (l/== constant {:type '[-> E T]
+                                                :value (with-meta (fn [arg] ; TODO: Same as above.
+                                                                    (println "Testing if" arg "was done in a" wordform "way."))
+                                                         {:print-as (symbol wordform)})}))]
+                    [(l/== cat "v")
+                     (l/== hypertag (l/partial-map {:head (l/partial-map {:trans ["false"]})}))
+                     (l/project [wordform]
+                                (l/== constant {:type '[-> E T]
+                                                :value (with-meta (fn [arg] ; TODO: Some more shake and bake.
+                                                                    (println "Testing if "arg "was doing" wordform "."))
+                                                         {:print-as (symbol wordform)})}))])))
+
+(defn amb-syntax-grammar [hypertag constant]
+  (let [and {:type '[-> T [-> T T]]
+             :value (with-meta (fn [x]
+                                 (fn [y]
+                                   (and x y)))
+                      {:print-as 'and})}
+        or {:type '[-> T [-> T T]]
+            :value (with-meta (fn [x]
+                                (fn [y]
+                                  (or x y)))
+                     {:print-as 'or})}
+        not {:type '[-> T T]
+             :value (with-meta not
+                      {:print-as 'not})}
+        imp {:type '[-> T [-> T T]]
+             :value (with-meta (fn [x]
+                                 (fn [y]
+                                   (or (not x) y)))
+                      {:print-as 'imp})}
+        top {:type 'T
+             :value true}
+        bot {:type 'T
+             :value false}
+        forall {:type '[-> [=> E T] T]
+                :value (with-meta (fn [pred]
+                                    (println "Checking that" pred "holds for everything."))
+                         {:print-as 'forall})}
+        exists {:type '[-> [=> E T] T]
+                :value (with-meta (fn [pred]
+                                    (println "Checking that" pred "holds for something."))
+                         {:print-as 'exists})}]
+    (l/fresh [unamb-syntax-constant simple-semantics-constant cat cats]
+             (unamb-syntax-grammar hypertag unamb-syntax-constant)
+             (l/== hypertag (l/partial-map {:head (l/partial-map {:cat cats})}))
+             (l/membero cat cats)
+             (l/conde [(l/== cat "n")
+                       (simple-semantics-grammar hypertag simple-semantics-constant)
+                       (l/== constant {:type 'N
+                                       :amb-syntax->unamb-syntax unamb-syntax-constant
+                                       :amb-syntax->simple-semantics simple-semantics-constant})]
+                      [(l/== cat "adj")
+                       (simple-semantics-grammar hypertag simple-semantics-constant)
+                       (l/== constant {:type '[-> N N]
+                                       :amb-syntax->unamb-syntax unamb-syntax-constant
+                                       :amb-syntax->simple-semantics simple-semantics-constant})]
+                      [(l/== cat "v")
+                       (l/== (l/partial-map {:head (l/partial-map {:trans ["false"]})})
+                             hypertag)
+                       (simple-semantics-grammar hypertag simple-semantics-constant)
+                       (n/fresh [S x]
+                                (l/== constant {:type '[-> NP S]
+                                                :amb-syntax->unamb-syntax unamb-syntax-constant
+                                                :amb-syntax->simple-semantics ['llam (n/tie S ['app S ['llam (n/tie x ['app simple-semantics-constant x])]])]}))]
+                      [(l/== cat "det")
+                       (l/fresh [det-type det-types]
+                                (l/== hypertag (l/partial-map {:head (l/partial-map {:det_type det-types})}))
+                                (l/membero det-type det-types)
+                                (l/conde [(l/== det-type "indef")
+                                          (n/fresh [p q x]
+                                                   (l/== constant {:type '[-> N NP]
+                                                                   :amb-syntax->unamb-syntax unamb-syntax-constant
+                                                                   :amb-syntax->simple-semantics ['llam (n/tie p ['llam (n/tie q ['app exists ['ilam (n/tie x
+                                                                                                                                                            ['app ['app and ['app p x]] ['app q x]])]])])]}))]))]))))
