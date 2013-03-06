887666f51ea8039b409af262d3f78d29b7974b32
Jiri Marsik <jiri.marsik89@gmail.com>
2013-03-06 11:20:20 +0100

    Type checker running backwards

--- src/acg_clj/core.clj
+++ src/acg_clj/core.clj
@@ -19,77 +19,43 @@
      (mergeo l1 t2 t)))
 
 (l/defne lookupo [c x tx]
-  ([[[x tx] . _] x tx])
-  ([[[y ty] . rc] x tx]
-     (n/hash x y)
-     (lookupo c x tx)))
+  ([[[x tx] . _] _ _])
+  ([[[y ty] . rc] _ _]
+     (l/!= x y)
+     (lookupo rc x tx)))
 
-(l/defne typeo-new [ic lc x t]
-  ([ic [] c t]
-     (l/== c (l/partial-map {:type t})))
-  ([ic [] v t]
-     (lookupo ic v t))
-  ([ic [[v t]] v t])
-  ([ic lc [lam binder] [arrow at bt]]
-     (n/fresh [a]
-              (l/fresh [b nic nlc]
-                       (l/== binder (n/tie a b))
-                       (l/conde [(l/== lam 'llam)
-                                 (l/== arrow '->)
-                                 (l/conso [a at] lc nlc)
-                                 (l/== ic nic)]
-                                [(l/== lam 'ilam)
-                                 (l/== arrow '=>)
-                                 (l/== lc nlc)
-                                 (l/conso [a at] ic nic)])
-                       (n/hash a ic)
-                       (n/hash a lc)
-                       (typeo-new nic nlc b bt))))
-  ([ic c ['app f a] t]
-     (l/fresh [c1 c2 ft at]
+(l/defne typeo [c ic lc x t]
+  ([_ _ _ _ _]
+     (l/conde [(lookupo c x :l)
+               (l/== lc [[x t]])]
+              [(lookupo c x :i)
+               (lookupo ic x t)]))
+  ([_ _ _ [lam [v] b] [arrow vt bt]]
+     (l/fresh [nic nlc nc]
+              (l/conde [(l/== lam 'llam)
+                        (l/== arrow '->)
+                        (l/conso [v vt] lc nlc)
+                        (l/== ic nic)
+                        (l/conso [v :l] c nc)]
+                       [(l/== lam 'ilam)
+                        (l/== arrow '=>)
+                        (l/== lc nlc)
+                        (l/conso [v vt] ic nic)
+                        (l/conso [v :i] c nc)])
+              (typeo nc nic nlc b bt)))
+  ([_ _ _ ['app f a] _]
+     (l/fresh [lcf lca ft at]
               (l/conde [(l/== ft ['-> at t])
-                        (mergeo c1 c2 c)]
+                        (mergeo lcf lca lc)]
                        [(l/== ft ['=> at t])
-                        (l/== c1 c)
-                        (l/== c2 [])])
-              (typeo-new ic c1 f ft)
-              (typeo-new ic c2 a at))))
-
-(l/defne typeo [const ic lc x t]
-  ([const ic [] ['const c] t]
-     (l/membero [c t] const))
-  ([const ic [] ['ivar v] t]
-     (lookupo ic v t))
-  ([const ic [[v t]] ['lvar v] t])
-  ([const ic lc [lam binder] [arrow at bt]]
-     (n/fresh [a]
-              (l/fresh [b nic nlc]
-                       (l/== binder (n/tie a b))
-                       (l/conde [(l/== lam 'llam)
-                                 (l/== arrow '->)
-                                 (l/conso [a at] lc nlc)
-                                 (l/== ic nic)]
-                                [(l/== lam 'ilam)
-                                 (l/== arrow '=>)
-                                 (l/== lc nlc)
-                                 (l/conso [a at] ic nic)])
-                       (n/hash a ic)
-                       (n/hash a lc)
-                       (typeo const nic nlc b bt))))
-  ([const ic c ['app f a] t]
-     (l/fresh [c1 c2 ft at]
-              (l/conde [(l/== ft ['-> at t])
-                        (mergeo c1 c2 c)]
-                       [(l/== ft ['=> at t])
-                        (l/== c1 c)
-                        (l/== c2 [])])
-              (typeo const ic c1 f ft)
-              (typeo const ic c2 a at))))
+                        (l/== lcf lc)
+                        (l/== lca [])])
+              (typeo c ic lcf f ft)
+              (typeo c ic lca a at))))
 
 
 
-
-(defn substo [e new a out]
+#_(defn substo [e new a out]
   (l/conde
     [(l/== ['var a] e) (l/== new out)]
     [(l/fresh [y]
