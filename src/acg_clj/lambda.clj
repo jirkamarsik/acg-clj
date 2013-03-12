c024c87618667b6ad87ee4fe73842facc9f3168d
Jiri Marsik <jiri.marsik89@gmail.com>
2013-03-12 14:18:45 +0100

    Comments for lambda core

--- src/acg_clj/lambda.clj
+++ src/acg_clj/lambda.clj
@@ -1,7 +1,11 @@
 (ns acg-clj.lambda
+  "Lambda calculus foundations (type inference, reduction)."
   (:require [clojure.core.logic :as l]))
 
-(l/defne mergeo [l1 l2 l]
+(l/defne ^{:doc "A relation ensuring that the list `l' can be formed
+  by merging the lists `l1' and `l2'. When merging two lists, any of
+  the two heads can be the head of the new list."} mergeo
+  [l1 l2 l]
   ([[] l2 l2])
   ([[h1 . t1] [] [h1 . t1]])
   ([[h1 . t1] [h2 . t2] [h1 . t]]
@@ -9,18 +13,28 @@
   ([[h1 . t1] [h2 . t2] [h2 . t]]
      (mergeo l1 t2 t)))
 
-(l/defne lookupo [c x tx]
+(l/defne ^{:doc "A relation ensuring that the alist `c' associates the
+  value `tx' to the key `x'."} lookupo
+  [c x tx]
   ([[[x tx] . _] _ _])
   ([[[y ty] . rc] _ _]
      (l/!= x y)
      (lookupo rc x tx)))
 
-(l/defne typeo [c ic lc x t]
+;; TODO: Should ensure consistency of `c' with `ic' and `lc'.
+(l/defne ^{:doc "A relation ensuring that the lambda term `x' (in its
+  tagged representation) has type `t', given the contexts `ic' and
+  `lc' for variables provided by intuitionistic and linear
+  intuitionistic lambdas, where `c' is an alist from variable names to the
+  keywords :i and :l which disambiguates which variable refers to a
+  variable in the intuitionistic or linear context."} typeo
+  [c ic lc x t]
   ([_ _ _ ['var v] _]
-     (l/conde [(lookupo c v :l)
-               (l/== lc [[v t]])]
-              [(lookupo c v :i)
-               (lookupo ic v t)]))
+     (l/conde [(lookupo c v :i)
+               (lookupo ic v t)
+               (l/== lc [])]
+              [(lookupo c v :l)
+               (l/== lc [[v t]])]))
   ([_ _ _ [lam [v] b] [arrow vt bt]]
      (l/fresh [nic nlc nc]
               (l/conde [(l/== lam 'llam)
