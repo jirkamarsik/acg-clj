5628264be27640875f686f6a29fad10ace5d1098
Jiri Marsik <jiri.marsik89@gmail.com>
2013-03-20 14:55:29 +0100

    Hmm... upgrades.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [prismatic/plumbing "0.0.1"]
-                 [org.clojure/core.logic "0.8.0-rc2"]])
+                 [org.clojure/core.logic "0.8.1"]])
